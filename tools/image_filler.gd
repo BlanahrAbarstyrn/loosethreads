905dfb86d7d440cd84e6cca2da1f949f0386947c
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-05-29 19:51:16 -0400

    set up naming convention, create image filler tool

---
+++ tools/image_filler.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorScript
+
+const PATH: String = "res://assets/emojis/"
+const RES_PATH: String = "res://resources/image_files_list.tres"
+
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run() -> void:
+	var dir: DirAccess = DirAccess.open(PATH)
+	var ifl: ImageFilesListResource = ImageFilesListResource.new()
+	
+	if dir:
+		var files: PackedStringArray = dir.get_files()
+		for fn in files:
+			ifl.add_file(PATH + fn)
+	ResourceSaver.save(ifl, RES_PATH)
